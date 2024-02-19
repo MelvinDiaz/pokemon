921c0278e69115cd67a13a388f7fdf0d54b21ffb
MelvinDiaz <melvinayaladiaz@gmail.com>
2024-02-19 14:36:37 -0600

    Add: dependencies and basic pokemons page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Pokemons from "./pages/pokemons/Pokemons"
 function App() {
 
   return(
-    <div>
+    <>
       <h1>App</h1>
-    </div>
+      <Pokemons/>
+    </>
   )
 }
 
---
+++ src/pages/pokemons/Pokemons.tsx
@@ -0,0 +1,7 @@
+
+export default function Pokemons(){
+
+    return (
+        <h1>Pokemons</h1>
+    )
+}
